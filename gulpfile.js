d520c9d279fcb6634f93ea13f29d70e46f566266
Andres Zapata <me@azapata.io>
2016-12-09 18:33:07 -0500

    :tada: Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const del = require('del');
+const gulp = require('gulp');
+const elixir = require('laravel-elixir');
+const webpack = require('laravel-elixir-webpack-official');
+
+elixir.config.assetsPath = 'src/';
+elixir.config.publicPath = 'dist/';
+
+const Task = elixir.Task;
+
+Elixir.extend('remove', function(path) {
+
+    new Task('remove', function() {
+        console.log(`Current directory: ${process.cwd()}`);
+        return del(path);
+    });
+});
+
+elixir(mix => {
+
+    mix.remove([ 'public/build']);
+
+    mix.sass('app.scss')
+
+        .webpack('app.js');      
+})
